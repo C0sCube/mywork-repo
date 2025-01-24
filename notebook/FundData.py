7317ee2433294b0fc45dc1e69942b5fd1938d395
CosCube <kaustubh0fnse@gmail.com>
2025-01-24 18:02:43 +0530

    one step towards clear code

--- notebook/FundData.py
+++ notebook/FundData.py
@@ -2,19 +2,20 @@ import re
 import os
 import pandas as pd
 import numpy as np
+from pdfParse import Reader
 
 
-class Samco:
+class Samco(Reader):
     
     #FUND NAME DETAILS order-> flag, regex_fund_name, font_size, font_color
-    fund_data =  [[25,20],r"^(samco|tata).*fund$",20.0]
+    fund_data =  [[25,20],r"^(samco|tata).*fund$",20.0,[-1]]
     content_bbox = [(35,120,250,765)]
     
     #CONTENT MANIPULATION PARAMS
-    data_cond = [[9.0,8.0],-1,20.0] #sizes, color, set_size
+    data_conditions = [[9.0,8.0],-1,20.0] #sizes, color, set_size
     
-    def __init__(self):
-        pass
+    def __init__(self, path: str,dry:str,fin:str):
+        super().__init__(path,dry,fin)
     
     def return_required_header(self,string: str):
             replace_key = string
@@ -222,23 +223,75 @@ class Samco:
         
         else:
             return self.__return_dummy_data(string,data)
-        
-        
+             
+class Tata(Reader):
+    
+    #FUND NAME DETAILS order-> flag, regex_fund_name, font_size, font_color
+    fund_data = [[25,20,0,16],r"^(samco|tata|canara).*",12,[-1]]
+    content_bbox = [(0,50,160,750)]
+    
+    #CONTENT MANIPULATION PARAMS
+    data_conditions = [[5.0,6.0,8.0], -15570765, 20.0]
     
     
+    def __init__(self, path: str,dry:str,fin:str):
+        super().__init__(path,dry,fin)
+    
+    def return_required_header(self,string: str):
+            replace_key = string
+            if re.match(r'^nav.*', string, re.IGNORECASE):
+                replace_key = "nav"
+            elif re.match(r"^market", string, re.IGNORECASE):
+                replace_key = "market_capital"  
+            elif re.match(r"^assets", string, re.IGNORECASE):
+                replace_key = "assets_under_management"
+            elif re.match(r"^fund", string, re.IGNORECASE):
+                replace_key = "fund_manager" 
+            elif re.match(r"^scheme", string, re.IGNORECASE):
+                replace_key = "scheme_details" 
+            elif re.match(r"^investment", string, re.IGNORECASE):
+                replace_key = "investment_objective"
+            elif re.match(r"^quanti", string, re.IGNORECASE):
+                replace_key = "quantitative_data"
+            elif re.match(r"^portfolio", string, re.IGNORECASE):
+                replace_key = "portfilio" 
+            elif re.match(r"^industry", string, re.IGNORECASE):
+                replace_key = "industry_allocation_of_equity"       
+            return replace_key
     
     
+    #REGEX FUNCTIONS
+    def __extract_inv_data(self,key:str, data:list):
+        return
     
+    def __extract_fund_data(self,key:str, data:list):
+        return
     
+    def __extract_nav_data(self,key:str, data:list):
+        return
     
+    def __extract_turn_data(self,key:str, data:list):
+        return
     
+    def __extract_bench_data(self,key:str, data:list):
+        return
     
+    def __extract_load_data(self,key:str, data:list):
+        return
     
+    def __extract_aum_data(self,key:str, data:list):
+        return
     
+    def __extract_trkerr_data(self,key:str, data:list):
+        return
     
+    def __extract_volat_data(self,key:str, data:list):
+        return
     
-    
-    
-    
-    
+    def __extract_dum_data(self,key,data:list):
+        return
+
+
+    def match_regex_to_content(self, string:str, data:list, *args):
+        return 
     
---
+++ notebook/helper.py
@@ -0,0 +1,141 @@
+import os, re
+import pprint
+import pandas as pd
+import numpy as np
+import fitz
+import pdfplumber
+
+class Helper:
+    
+    
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def get_fund_paths(path:str):
+        mutual_fund_paths = {}
+
+        for root, dirs, files in os.walk(path):
+            for name in files:
+                if name.endswith((".pdf")):
+                    tmp = root.split("\\")
+                    key = tmp[-1]
+                    value = rf'{root}\{name}'
+                    mutual_fund_paths[key] = value
+        
+        return mutual_fund_paths
+
+    
+    @staticmethod
+    def get_all_pdf_data(path:str):
+    
+        doc = fitz.open(path)
+        count = doc.page_count
+        all_blocks = list()
+
+        for pgn in range(count):
+            page = doc[pgn]
+            
+            blocks = page.get_text('dict')['blocks']
+            images = page.get_images()
+            filtered_blocks = [block for block in blocks if block['type']==0]
+            sorted_blocks = sorted(filtered_blocks, key=lambda x: x['bbox'][1])
+            all_blocks.append({
+                "pgn":pgn,
+                "blocks":sorted_blocks,
+                "images": images
+            })
+            
+            #draw lines
+            
+            lines = fitz.Rect()
+            
+        doc.close()
+        
+        return all_blocks
+    
+    """Open the pdf , draw lines on the mentioned pages
+    Args:input_pdf_path(str) , output_pdf_path (str)
+    Returns: nothing, a new pdf created"""
+    
+    @staticmethod
+    def draw_lines_on_pdf(pdf_path: str, lines: list, rects:list, pages:list,output_path: str):
+        doc = fitz.open(pdf_path)
+        
+        
+        for page_number, page in enumerate(doc, start=1):
+            
+            height = page.rect.height
+            width  = page.rect.width
+            if page_number in pages:
+                
+                # Start drawing on the page
+                for line in lines:
+                    start, end = line
+                    x1, y1 = start
+                    x2, y2 = end
+                    page.draw_line((x1, y1), (x2, y2))
+                    #page.draw_rect((0,20,250,1000))
+                
+                # Start drawing on the page
+                for rec in rects:
+                    x0, y0, x1, y1 = rec  
+                    rect = fitz.Rect(x0, y0, x1, height) 
+
+                    # Set the rectangle's fill and stroke color
+                    shape = page.new_shape()
+                    shape.draw_rect(rect)  
+                    shape.finish(color=(1,0,0), fill=(1, 0.75, 0.8), width=0.8, fill_opacity = .3)  # Pink fill, no border color
+                    shape.commit()
+            
+
+
+        doc.save(output_path)
+        print(f"Modified PDF saved to: {output_path}")
+        #open the file on screen
+        import subprocess
+        subprocess.Popen([output_path],shell=True)
+    
+   
+    """Open the pdf , get all text data and blocks and draw a boundary along each boundary boxes
+    Args:input_pdf_path(str) , output_pdf_path (str)
+    Returns: nothing, a new pdf created"""
+    
+    @staticmethod
+    def draw_boundaries_on_lines(input_pdf_path:str, path:str):
+        # Open the PDF file
+        doc = fitz.open(input_pdf_path)
+        for page in doc:
+            blocks = page.get_text("dict")["blocks"]
+            for block in blocks:
+                if "lines" in block:
+                    for line in block["lines"]:
+                        
+                        bbox = line["bbox"]  # The bbox is now directly accessible from the line
+                        page.draw_rect(bbox, color=(.4, 0.647, 0.0), width=1.5, overlay=False)
+        
+        
+        output_path = path.replace('.pdf', '_line_highlighted.pdf')
+        doc.save(output_path)
+        doc.close()
+        
+    """Open the pdf , get all text data and blocks and draw a boundary along each boundary boxes
+    Args:input_pdf_path(str) , output_pdf_path (str)
+    Returns: nothing, a new pdf created"""
+    
+    @staticmethod
+    def draw_boundaries_on_pdf(input_pdf_path:str, path:str):
+        # Open the PDF file
+        doc = fitz.open(input_pdf_path)
+        for page in doc:
+            blocks = page.get_text("blocks")  # Get the blocks of text on the page
+            for block in blocks:
+                bbox = block[:4]  # The bbox is the first four elements of the block
+                # Draw a rectangle with an orange border around the bbox
+                page.draw_rect(bbox, color=(1.0, 0.647, 0.0), width=1.5, overlay=False)
+        
+        # Save the modified document to a new file
+        
+        output_path = path.replace('.pdf', '_block_highlighted.pdf')
+        doc.save(output_path)
+        doc.close()
