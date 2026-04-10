1e3f63f459d9b61c186f7de78f6405416038fd33
Kaustubh Keny <kaustubh.keny@cogencis.com>
2026-04-10 08:05:46 +0530

    Hello Okay

--- amfii_monitor/kill.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ==============================
-echo Stopping Python Application
-echo ==============================
-
-taskkill /FI "WINDOWTITLE eq PY_APP_RUNNER*" /T /F
-
-echo Done.
-pause
